7bf4270926315a69aa252486697446617d716090
dwmanguiat <dwmanguiat@gmail.com>
2021-08-26 22:39:16 -0400

    more sql woo

--- SQL/schema/node.sql
+++ SQL/schema/node.sql
@@ -1,7 +1,9 @@
 CREATE TABLE `node` (
-	`node_id` INT NOT NULL AUTO_INCREMENT,
 	`id` VARCHAR(36) NOT NULL,
 	`name` VARCHAR(100),
 	`office` VARCHAR(100),
-	PRIMARY KEY (`node_id`)
+	PRIMARY KEY (`id`)
+
 );
+
+CREATE INDEX idx_node_id on 'node' (id);
--- SQL/schema/transfer.sql
+++ SQL/schema/transfer.sql
@@ -1,9 +1,17 @@
-CREATE TABLE `transfer` (
-	`transfer_id` INT NOT NULL AUTO_INCREMENT,
-	`timestamp` TIMESTAMP NOT NULL,
-	`type` VARCHAR(6) NOT NULL,
-	`duration` INT NOT NULL,
-	`source_id` VARCHAR(36),
-	`destination_id` VARCHAR(36),
-	PRIMARY KEY (`transfer_id`)
+CREATE TABLE 'transfer' (
+	'transfer_id' INT NOT NULL AUTO_INCREMENT,
+	'timestamp' TIMESTAMP NOT NULL,
+	'type' VARCHAR(6) NOT NULL,
+	'duration' INT NOT NULL,
+	'source_id' VARCHAR(36),
+	'destination_id' VARCHAR(36),
+	PRIMARY KEY ('transfer_id'),
+	FOREIGN KEY ('source_id') REFERENCES node(id),
+	FOREIGN KEY ('destination_id') REFERENCES node(id)
+
 );
+
+CREATE idx_transfer_transfer_id on transfer (transfer_id); --MySQL does not have this option, but in SQL server for examle this would be a clustered index
+CREATE idx_transfer_source_id on transfer (source_id);
+CREATE idx_transfer_destination_id on transfer (destination_id);
+CREATE idx_transfer_timestamp on transfer (timestamp)
